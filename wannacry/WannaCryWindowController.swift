b5160ae822e314a61817d2b7d4ea3a3a5595ced6
Ach1n <928155617@qq.com>
2017-05-15 20:15:17 +0800

    1

---
+++ wannacry/WannaCryWindowController.swift
@@ -0,0 +1,20 @@
+//
+//  WannaCryWindowController.swift
+//  WannaCry
+//
+//  Created by 冯贵辰 on 2017/5/15.
+//  Copyright © 2017年 Achen. All rights reserved.
+//
+
+import Cocoa
+
+class WannaCryWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        self.window?.titleVisibility = .hidden
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
