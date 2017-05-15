e9a68b6dab6f7a6bddebae3fb219cff2fcc30e27
Ach1n <928155617@qq.com>
2017-05-15 19:06:36 +0800

    Initial Commit

---
+++ wannacry/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  wannacry
+//
+//  Created by 冯贵辰 on 2017/5/15.
+//  Copyright © 2017年 Achen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ wannacry/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  wannacry
+//
+//  Created by 冯贵辰 on 2017/5/15.
+//  Copyright © 2017年 Achen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
